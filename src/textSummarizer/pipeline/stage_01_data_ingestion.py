369ad13da8c2a33c2a29844f7d314defdfcd7a7f
Rishi Bakshi <guitarguyrishi@gmail.com>
2023-08-15 19:57:36 +0530

    data ingestion completed

---
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,14 @@
+from src.textSummarizer.config.configurations import ConfigurationManager
+from src.textSummarizer.components.data_ingestion import DataIngestion
+from src.textSummarizer.logging import logger
+
+class DataIngestionTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config=ConfigurationManager()
+        data_ingestion_config=config.get_data_ingestion_config()
+        data_ingestion=DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
